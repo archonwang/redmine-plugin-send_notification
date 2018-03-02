09b26fd3c0f548fa27668aaa058f412a77322cbf
Sergey Vershinin <accountwm@gmail.com>
2018-03-02 08:43:49 +0300

    Code refactoring

--- lib/send_notification/issue_patch.rb
+++ lib/send_notification/issue_patch.rb
@@ -13,26 +13,32 @@ module SendNotification
 
         def check_before_send_recipient_email
           unless recipient_email.blank?
-            issue_status_confirmed_id = Setting.plugin_send_notification["issue_status_confirmed_id"].to_i
-            issue_status_completed_id = Setting.plugin_send_notification["issue_status_completed_id"].to_i
-            issue_status_in_work_id = Setting.plugin_send_notification["issue_status_in_work_id"].to_i
-            issue_status_in_stop_id = Setting.plugin_send_notification["issue_status_in_stop_id"].to_i
             # Task confirmed
-            if notify? && (status_id_changed? && status_id == issue_status_confirmed_id)
-              TechMailer.send_issue_accept(self).deliver_now
+            TechMailer.send_issue_accept(self).deliver_now if issue_status_confirmed?
             # Task Completed
-            elsif notify? && (status_id_changed? && status_id == issue_status_completed_id)
-              TechMailer.send_issue_end(self).deliver_now
-            #Task due date change
-            elsif notify? && (start_date_changed? || due_date_changed?) &&
-                (
-                    status_id == issue_status_confirmed_id ||
-                    status_id == issue_status_in_work_id ||
-                    status_id == issue_status_in_stop_id
-                )
-              TechMailer.send_issue_change(self).deliver_now
-            end
+            TechMailer.send_issue_end(self).deliver_now if issue_status_completed?
+            # Task due date change
+            TechMailer.send_issue_change(self).deliver_now if issue_change_dates?
           end
+        end
+
+        def issue_status_confirmed?
+          issue_status_confirmed_id = Setting.plugin_send_notification["issue_status_confirmed_id"].to_i
+          (notify? && (status_id_changed? && status_id == issue_status_confirmed_id)) || false
+        end
+
+        def issue_status_completed?
+          issue_status_completed_id = Setting.plugin_send_notification["issue_status_completed_id"].to_i
+          notify? && (status_id_changed? && status_id == issue_status_completed_id) || false
+        end
+
+        def issue_change_dates?
+          issue_status_confirmed_id = Setting.plugin_send_notification["issue_status_confirmed_id"].to_i
+          issue_status_in_work_id = Setting.plugin_send_notification["issue_status_in_work_id"].to_i
+          issue_status_in_stop_id = Setting.plugin_send_notification["issue_status_in_stop_id"].to_i
+          (notify? && (start_date_changed? || due_date_changed?) &&
+              (status_id == issue_status_confirmed_id || status_id == issue_status_in_work_id || status_id == issue_status_in_stop_id)
+          ) || false
         end
       end
     end
