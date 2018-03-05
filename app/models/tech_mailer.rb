30d0d81208b02b30eb0393abc4f70a643c2d07b3
Sergey Vershinin <accountwm@gmail.com>
2018-03-05 15:14:02 +0300

    All text in locales

--- app/models/tech_mailer.rb
+++ app/models/tech_mailer.rb
@@ -8,21 +8,21 @@ class TechMailer < ActionMailer::Base
     @text = issue.subject
     @to = extract_email_to_array(issue.recipient_email)
     @issue = issue
-    mail to: @to, subject: "Задача включена в план", from: Setting.mail_from
+    mail to: @to, subject: l(:email_body_subject_issue_confirmed), from: Setting.mail_from
   end
 
   def send_issue_change(issue)
     @text = issue.subject
     @to = extract_email_to_array(issue.recipient_email)
     @issue = issue
-    mail to: @to, subject: "Плановые сроки выполнения задачи изменились", from: Setting.mail_from
+    mail to: @to, subject: l(:email_body_subject_issuen_change), from: Setting.mail_from
   end
 
   def send_issue_completed(issue)
     @text = issue.subject
     @to = extract_email_to_array(issue.recipient_email)
     @issue = issue
-    mail to: @to, subject: "Задача выполнена", from: Setting.mail_from
+    mail to: @to, subject: l(:email_body_subject_issue_completed), from: Setting.mail_from
   end
 
   def extract_email_to_array(str)
